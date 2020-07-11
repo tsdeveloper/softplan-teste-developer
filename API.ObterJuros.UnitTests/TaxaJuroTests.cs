9ae65f999d48c0948c8e995a0a01a092a1c8b451
thiago <ts.analista@gmail.com>
2020-07-11 15:26:14 -0300

    fix: resolve test unit entity taxa juros

---
+++ API.ObterJuros.UnitTests/TaxaJuroTests.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Web.Http;
+using System.Web.Http.Results;
+using Common.Factory;
+using Core.Entities;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace API.ObterJuros.Tests
+{
+    public class TaxaJuroTests
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(0)]
+        [InlineData(null)]
+        public void TaxaJuro_ObterTempo_DeveSerMaiorQueZero(int? tempo)
+        {
+            #region Arrange
+
+            var taxaJuro = new TaxaJuro().BuildFactory(100, 0.01M, tempo.Value);
+
+            #endregion
+
+            #region Act
+
+            var resultado = taxaJuro.Tempo;
+
+            #endregion
+
+            #region Assert
+
+            resultado.Should().BeGreaterOrEqualTo(1);
+
+            #endregion
+        }
+        
+        [Fact]
+        public void TaxaJuro_ObterValorTaxaJuro_DeveSerMaiorQueZero()
+        {
+            #region Arrange
+
+            var taxaJuro = new TaxaJuro().BuildFactory();
+
+            #endregion
+
+            #region Act
+
+            var resultado = taxaJuro.ValorJuro;
+
+            #endregion
+
+            #region Assert
+
+            resultado.Should().BeGreaterOrEqualTo(0.01M);
+
+            #endregion
+        }
+    }
+}
---
+++ Common/Factory/TaxaJuroFactory.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities;
+
+namespace Common.Factory
+{
+    public static class TaxaJuroFactory
+    {
+        public static TaxaJuro BuildFactory(this TaxaJuro taxaJuro)
+        {
+            return new TaxaJuro
+            {
+               ValorInicial = 100,
+                ValorJuro = 0.01M,
+                Tempo = 5,
+            };
+        }
+        
+        public static TaxaJuro BuildFactory(this TaxaJuro taxaJuro, decimal valorInicial, decimal valorJuro, int? tempo)
+        {
+            return new TaxaJuro
+            {
+                ValorInicial = valorInicial,
+                ValorJuro = valorJuro,
+                Tempo = tempo ?? 0,
+            };
+        }
+        
+        
+    }
+}
