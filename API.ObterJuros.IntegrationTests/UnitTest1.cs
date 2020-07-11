3c57cc1d716339f3907d03f259162d7bced86620
thiago <ts.analista@gmail.com>
2020-07-11 15:28:13 -0300

    fix: add integration tests

---
+++ API.ObterJuros.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace API.ObterJuros.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
